d52badbd4e6cd39c1d5e2403791c38167736eec8
hanahh080601 <hanlehngoc080601@gmail.com>
2022-07-19 10:47:15 +0700

    [Task] Build UI for sentiment classification using Jinja2.

--- sentiment-classification/sentiment_classification/main.py
+++ sentiment-classification/sentiment_classification/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routes.comment import comment
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(comment)
--- sentiment-classification/sentiment_classification/routes/comment.py
+++ sentiment-classification/sentiment_classification/routes/comment.py
@@ -1,19 +1,27 @@
+from fastapi import Request, Form
+from fastapi.responses import HTMLResponse
 from fastapi import APIRouter
 from database.database import conn, add_comment
 from schemas.comment import commentEntity, commentEntities
 from predict import prediction
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 comment = APIRouter()
+comment.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@comment.get('/')
-async def list_all_comments():
-    return commentEntities(conn.hanlhn.comments.find())
+@comment.get('/', response_class=HTMLResponse)
+async def list_all_comments(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "list_comments": commentEntities(conn.hanlhn.comments.find())})
+    #return commentEntities(conn.hanlhn.comments.find())
 
-@comment.post('/')
-async def predict_sentiment(comment: str):
+@comment.post('/', response_class=HTMLResponse)
+async def predict_sentiment(request: Request, comment: str = Form()):
     try:
         pred = prediction.predict(comment)
         await add_comment(comment, pred)
     except Exception as e:
         print(e)
-    return pred
+    #return pred
+    return templates.TemplateResponse("index.html", {"request": request, "list_comments": commentEntities(conn.hanlhn.comments.find())})
